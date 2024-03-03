ad6dd10c87f181ac1e6115116c15e2b80927b44e
tangkikodo <allmonday@126.com>
2024-03-03 08:20:32 +0800

    update readme.

---
+++ examples/0_demo.py
@@ -0,0 +1,53 @@
+import asyncio
+from pydantic import BaseModel
+from pydantic_resolve import Resolver
+
+comments_table = [
+    dict(id=1, blog_id=1, content='its interesting'),
+    dict(id=2, blog_id=1, content='i dont understand'),
+    dict(id=3, blog_id=2, content='why? how?'),
+    dict(id=4, blog_id=2, content='wow!'),
+]
+
+async def query_comments(blog_id: int):
+    print(f'run query - {blog_id}')
+    return [c for c in comments_table if c['blog_id'] == blog_id]
+
+class Comment(BaseModel):
+    id: int
+    content: str
+
+class Blog(BaseModel):
+    id: int
+    title: str
+
+    comments: list[Comment] = []
+    async def resolve_comments(self):
+        return await query_comments(self.id)
+
+class MyBlogSite(BaseModel):
+    blogs: list[Blog]
+
+
+async def single():
+    blog = Blog(id=1, title='what is pydantic-resolve')
+    blog = await Resolver().resolve(blog)
+    print(blog)
+
+
+async def batch():
+    my_blog_site = MyBlogSite(
+        blogs = [
+            Blog(id=1, title='what is pydantic-resolve'),
+            Blog(id=2, title='what is composition oriented development pattarn'),
+        ]
+    )
+    my_blog_site = await Resolver().resolve(my_blog_site)
+    print(my_blog_site.model_dump_json(indent=4))
+
+
+async def main():
+    await single()
+    await batch()
+
+asyncio.run(main())
---
+++ examples/0_demo_loader.py
@@ -0,0 +1,46 @@
+import asyncio
+from pydantic import BaseModel
+from pydantic_resolve import Resolver, build_list, LoaderDepend
+
+comments_table = [
+    dict(id=1, blog_id=1, content='its interesting'),
+    dict(id=2, blog_id=1, content='i dont understand'),
+    dict(id=3, blog_id=2, content='why? how?'),
+    dict(id=4, blog_id=2, content='wow!'),
+]
+
+async def blog_to_comments_loader(blog_ids: list[int]):
+    print(blog_ids)
+    return build_list(comments_table, blog_ids, lambda c: c['blog_id'])
+
+
+class Comment(BaseModel):
+    id: int
+    content: str
+
+class Blog(BaseModel):
+    id: int
+    title: str
+
+    comments: list[Comment] = []
+    def resolve_comments(self, loader=LoaderDepend(blog_to_comments_loader)):
+        return loader.load(self.id)
+
+class MyBlogSite(BaseModel):
+    blogs: list[Blog]
+
+async def batch():
+    my_blog_site = MyBlogSite(
+        blogs = [
+            Blog(id=1, title='what is pydantic-resolve'),
+            Blog(id=2, title='what is composition oriented development pattarn'),
+        ]
+    )
+    my_blog_site = await Resolver().resolve(my_blog_site)
+    print(my_blog_site.model_dump_json(indent=4))
+
+
+async def main():
+    await batch()
+
+asyncio.run(main())
